a04fde43d51f78a2d719ff5dac9ac33a59252c5e
Daniel <danielbanasen@gmail.com>
2019-05-29 22:16:15 +0800

    Updates

---
+++ precache-manifest.3104f3016277659cf44ddac9630b6d30.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640c2618597dd18017476fdefd5d80a7",
+    "url": "/starwarsapi/index.html"
+  },
+  {
+    "revision": "eebb5432e617ec36a0d2",
+    "url": "/starwarsapi/static/css/main.6991d2af.chunk.css"
+  },
+  {
+    "revision": "742561b2cae19395b09e",
+    "url": "/starwarsapi/static/js/2.5f570e04.chunk.js"
+  },
+  {
+    "revision": "eebb5432e617ec36a0d2",
+    "url": "/starwarsapi/static/js/main.11fd0646.chunk.js"
+  },
+  {
+    "revision": "460864e0b3a2c1204984",
+    "url": "/starwarsapi/static/js/runtime~main.a0f48d18.js"
+  },
+  {
+    "revision": "8ea9f747f4a83a830d04ed3e3b36754a",
+    "url": "/starwarsapi/static/media/arrow-left2.8ea9f747.svg"
+  },
+  {
+    "revision": "1188e2d3cf5ea50eef8a17c824e2afbe",
+    "url": "/starwarsapi/static/media/arrow-right2.1188e2d3.svg"
+  },
+  {
+    "revision": "a95bb137784dee08a108f08a9db1120c",
+    "url": "/starwarsapi/static/media/icons8-darth-vader.a95bb137.svg"
+  },
+  {
+    "revision": "49d9664dd7c47f907519914bd4286cc3",
+    "url": "/starwarsapi/static/media/make-group.49d9664d.svg"
+  },
+  {
+    "revision": "bc7637ea4baa00ffeed35f06f87ce4be",
+    "url": "/starwarsapi/static/media/sphere.bc7637ea.svg"
+  },
+  {
+    "revision": "9d5778696852ad91df7272aba3e320f4",
+    "url": "/starwarsapi/static/media/user.9d577869.svg"
+  }
+]);
