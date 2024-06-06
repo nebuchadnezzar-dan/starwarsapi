a9080883b053a49c497153a06e997e9db720919f
daniel <danielbanasen@gmail.com>
2024-06-06 19:31:20 +0800

    updated the api url

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import './sidebar.css';
+import React, { Component } from "react";
+import "./sidebar.css";
 
-import { ReactComponent as People } from '../../img/SVG/user.svg';
-import { ReactComponent as Species } from '../../img/SVG/make-group.svg';
-import { ReactComponent as Planets } from '../../img/SVG/sphere.svg';
+import { ReactComponent as People } from "../../img/SVG/user.svg";
+import { ReactComponent as Species } from "../../img/SVG/make-group.svg";
+import { ReactComponent as Planets } from "../../img/SVG/sphere.svg";
 
-import Auxillary from '../../hoc/Auxillary/Auxillary';
+import Auxillary from "../../hoc/Auxillary/Auxillary";
 
 class Sidebar extends Component {
   state = {
-    hide: true
+    hide: true,
   };
 
   componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
+    window.addEventListener("resize", this.updateDimensions);
   }
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
+    window.removeEventListener("resize", this.updateDimensions);
   }
   componentWillMount() {
     this.updateDimensions();
@@ -33,33 +33,33 @@ class Sidebar extends Component {
     const { onClicked, active } = this.props;
     return (
       <Auxillary>
-        <div className={`navigation ${this.state.hide ? 'hideNav' : null}`}>
+        <div className={`navigation ${this.state.hide ? "hideNav" : null}`}>
           <nav>
             <ul className="nav-style" onClick={onClicked}>
-              <li className={active === 'people' ? 'active' : ''}>
+              <li className={active === "people" ? "active" : ""}>
                 <p
                   className="nav-link people"
-                  data-url="https://swapi.co/api/people/?page=1"
+                  data-url="https://swapi.dev/api/people/?page=1"
                   data-active="people"
                 >
                   <People className="nav-icon" />
                   <span>People</span>
                 </p>
               </li>
-              <li className={active === 'species' ? 'active' : ''}>
+              <li className={active === "species" ? "active" : ""}>
                 <p
                   className="nav-link species"
-                  data-url="https://swapi.co/api/species/?page=1"
+                  data-url="https://swapi.dev/api/species/?page=1"
                   data-active="species"
                 >
                   <Species className="nav-icon" />
                   <span>Species</span>
                 </p>
               </li>
-              <li className={active === 'planets' ? 'active' : ''}>
+              <li className={active === "planets" ? "active" : ""}>
                 <p
                   className="nav-link planets"
-                  data-url="https://swapi.co/api/planets/?page=1"
+                  data-url="https://swapi.dev/api/planets/?page=1"
                   data-active="planets"
                 >
                   <Planets className="nav-icon" />
@@ -70,7 +70,7 @@ class Sidebar extends Component {
           </nav>
           <div className="footer">
             <p>
-              Powered by <a href="https://swapi.co/">swapi.co</a>
+              Powered by <a href="https://swapi.dev/">swapi.dev</a>
             </p>
             <hr />
             <div className="hideButton" onClick={this.onToggleNav}>
@@ -78,7 +78,7 @@ class Sidebar extends Component {
             </div>
           </div>
         </div>
-        <div className={`navigation ${!this.state.hide ? 'hideNav' : null}`}>
+        <div className={`navigation ${!this.state.hide ? "hideNav" : null}`}>
           <div className="showButton" onClick={this.onToggleNav}>
             &#9660;
           </div>
