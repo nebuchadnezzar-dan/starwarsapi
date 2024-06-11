a9d723f95f73101ef0afe96a4b8aadcb8cc383d8
daniel <danielbanasen@gmail.com>
2024-06-11 15:36:04 +0800

    sideBar conversion

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./sidebar.css";
 
 import { ReactComponent as People } from "../../img/SVG/user.svg";
@@ -7,85 +7,77 @@ import { ReactComponent as Planets } from "../../img/SVG/sphere.svg";
 
 import Auxillary from "../../hoc/Auxillary/Auxillary";
 
-class Sidebar extends Component {
-  state = {
-    hide: true,
-  };
+function Sidebar({ onClicked, active }) {
+  const [hide, setHide] = useState(true);
+  useEffect(function () {
+    window.addEventListener("resize", updateDimensions);
 
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
-  }
-  componentWillMount() {
-    this.updateDimensions();
-  }
-  updateDimensions = () => {
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  function updateDimensions() {
     if (window.innerWidth >= 700) {
-      this.setState({ hide: true });
+      setHide(true);
     }
-  };
-  onToggleNav = () => {
-    this.setState({ hide: !this.state.hide });
-  };
-  render() {
-    const { onClicked, active } = this.props;
-    return (
-      <Auxillary>
-        <div className={`navigation ${this.state.hide ? "hideNav" : null}`}>
-          <nav>
-            <ul className="nav-style" onClick={onClicked}>
-              <li className={active === "people" ? "active" : ""}>
-                <p
-                  className="nav-link people"
-                  data-url="https://swapi.dev/api/people/?page=1"
-                  data-active="people"
-                >
-                  <People className="nav-icon" />
-                  <span>People</span>
-                </p>
-              </li>
-              <li className={active === "species" ? "active" : ""}>
-                <p
-                  className="nav-link species"
-                  data-url="https://swapi.dev/api/species/?page=1"
-                  data-active="species"
-                >
-                  <Species className="nav-icon" />
-                  <span>Species</span>
-                </p>
-              </li>
-              <li className={active === "planets" ? "active" : ""}>
-                <p
-                  className="nav-link planets"
-                  data-url="https://swapi.dev/api/planets/?page=1"
-                  data-active="planets"
-                >
-                  <Planets className="nav-icon" />
-                  <span>Planets</span>
-                </p>
-              </li>
-            </ul>
-          </nav>
-          <div className="footer">
-            <p>
-              Powered by <a href="https://swapi.dev/">swapi.dev</a>
-            </p>
-            <hr />
-            <div className="hideButton" onClick={this.onToggleNav}>
-              &#9650;
-            </div>
-          </div>
-        </div>
-        <div className={`navigation ${!this.state.hide ? "hideNav" : null}`}>
-          <div className="showButton" onClick={this.onToggleNav}>
-            &#9660;
-          </div>
-        </div>
-      </Auxillary>
-    );
   }
+  function onToggleNav() {
+    setHide((hidden) => !hidden);
+  }
+
+  return (
+    <Auxillary>
+      <div className={`navigation ${hide ? "hideNav" : null}`}>
+        <nav>
+          <ul className="nav-style" onClick={onClicked}>
+            <li className={active === "people" ? "active" : ""}>
+              <p
+                className="nav-link people"
+                data-url="https://swapi.dev/api/people/?page=1"
+                data-active="people"
+              >
+                <People className="nav-icon" />
+                <span>People</span>
+              </p>
+            </li>
+            <li className={active === "species" ? "active" : ""}>
+              <p
+                className="nav-link species"
+                data-url="https://swapi.dev/api/species/?page=1"
+                data-active="species"
+              >
+                <Species className="nav-icon" />
+                <span>Species</span>
+              </p>
+            </li>
+            <li className={active === "planets" ? "active" : ""}>
+              <p
+                className="nav-link planets"
+                data-url="https://swapi.dev/api/planets/?page=1"
+                data-active="planets"
+              >
+                <Planets className="nav-icon" />
+                <span>Planets</span>
+              </p>
+            </li>
+          </ul>
+        </nav>
+        <div className="footer">
+          <p>
+            Powered by <a href="https://swapi.dev/">swapi.dev</a>
+          </p>
+          <hr />
+          <div className="hideButton" onClick={onToggleNav}>
+            &#9650;
+          </div>
+        </div>
+      </div>
+      <div className={`navigation ${!hide ? "hideNav" : null}`}>
+        <div className="showButton" onClick={onToggleNav}>
+          &#9660;
+        </div>
+      </div>
+    </Auxillary>
+  );
 }
 
 export default Sidebar;
